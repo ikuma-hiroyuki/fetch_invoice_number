4fb443ed3a3eebdecba4cf21e58f4824b81bb85c
ikuma <chemicalbondo@gmail.com>
2023-08-22 17:02:41 +0900

    対象企業の取得方法を変更した

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ def fetch_request(numbers: list[str],
     """
     APIにリクエストを発行し、レスポンスを返す
     :param version: APIのバージョン
-    :param numbers: 法人番号のリスト
+    :param numbers: 法人番号のリスト (最大10件)
     :param request_type: 01:CSV 形式/Shift-JIS, 02:CSV 形式/Unicode, 12:XML 形式/Unicode
     :param history: 変更履歴を取得するかどうか。0:取得しない, 1:取得する
     :return:
@@ -53,24 +53,54 @@ def write_csv_by_xml(xml_text: str):
             writer.writerow({"法人番号": number, "法人名": name})
 
 
-def get_corporate_number_list():
+def create_target_corporate_dict() -> dict[str, str]:
     """
-    csv/target.csvから法人番号のリストを取得する
-
-    リクエストが10件ずつしか受け入れないので、10件ずつ法人番号のリストを返す
+    csv/target.csvから法人番号と法人名の辞書を作成して返す
+    :return: 法人番号と法人名の辞書
     """
 
     with open(target_csv, "r", encoding="utf-8") as file:
-        fieldnames = ["法人番号"]
+        fieldnames = ["事業者名", "登録番号"]
         reader: csv.DictReader = csv.DictReader(file, fieldnames=fieldnames)
+        next(reader)
+        target_dict = {row["登録番号"].replace("-", "")[1:]: row["事業者名"] for row in reader}
+        return target_dict
 
-        corporate_numbers = []
-        for row in reader:
-            corporate_numbers.append(row["法人番号"])
-            if len(corporate_numbers) == 10:
-                yield corporate_numbers
-                corporate_numbers = []
-        yield corporate_numbers
+
+def get_corporate_number_list() -> list[list[str]]:
+    """
+    APIリクエストで取得できるデータ数が最大10件なので、法人番号のリストを10件ずつ返す
+    :return: 法人番号のリスト
+    """
+
+    target_dict = create_target_corporate_dict()
+    registration_numbers = []
+    for key in target_dict.keys():
+        registration_numbers.append(key)
+        if len(registration_numbers) == 9:
+            yield registration_numbers
+            registration_numbers = []
+    yield registration_numbers
+
+
+def create_corporate_dict() -> dict[str, dict[str, str]]:
+    """
+    法人番号と法人名、住所の辞書を作成してして返す
+    :return: 法人番号と法人名、住所の辞書
+    """
+
+    corporate_number_list = get_corporate_number_list()
+    corp_dict = {}
+    for corp_num in corporate_number_list:
+        xml_data = fetch_request(corp_num).text
+
+        root = xml.etree.ElementTree.fromstring(xml_data).findall(".//corporation")
+        for corp in root:
+            name = corp.find("name").text
+            number = corp.find("corporateNumber").text
+            address = f'{corp.find("prefectureName").text}{corp.find("cityName").text}{corp.find("streetNumber").text}'
+            corp_dict[number] = {"name": name, "address": address}
+    return corp_dict
 
 
 def remove_and_create_result_csv():
@@ -88,8 +118,11 @@ if __name__ == "__main__":
     load_dotenv()
     api_id = os.getenv("API_ID")
 
-    remove_and_create_result_csv()
-    corporate_number_list = get_corporate_number_list()
-    for corp_num in corporate_number_list:
-        xml_data = fetch_request(corp_num).text
-        write_csv_by_xml(xml_data)
+    # remove_and_create_result_csv()
+    # corporate_number_list = get_corporate_number_list()
+    # for corp_num in corporate_number_list:
+    #     xml_data = fetch_request(corp_num).text
+    #     write_csv_by_xml(xml_data)
+
+    d = create_corporate_dict()
+    print(d)
